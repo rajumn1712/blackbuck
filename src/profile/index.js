d0b24e45c44b92e69efeeb81f74b2f7a323b95f9
vishnutrimurthulu <vishnutrimurthulu.ziraff@gmail.com>
2020-11-10 16:23:49 +0530

    profile banner designs

--- src/profile/index.js
+++ src/profile/index.js
@@ -13,6 +13,7 @@ import './profilestyle.css'
 import Title from 'antd/lib/typography/Title';
 import Paragraph from 'antd/lib/skeleton/Paragraph';
 import AvatarImage from '../styles/images/avatar.png';
+import PremiumBadge from '../styles/images/premiumbadge.svg'
 const { Meta } = Card;
 
 const { TabPane } = Tabs;
@@ -30,13 +31,13 @@ class Profile extends Component {
                     <Col xs={24} sm={16} md={16} lg={18} xl={18}>
                         <div className="coverpage">
                             <img src={profilebanner} alt="profilecover" />
+                            <span className="premium-badge"><img src={PremiumBadge} /></span>
                         </div>
-                        <div className="user-statistic">
-                            <Statistic title="Friends" value={58} />
-                            <Statistic title="Groups" value={8} />
+                        <div className="user-statistic ">
+                            <Statistic title="Friends" className="afterline" value={58} />
+                            <Statistic className="afterline" title="Groups" value={8} />
                             <Statistic title="Posts" value={10} />
                             <Card className="user-banner"
-
                             >
                                 <Meta
                                     avatar={
@@ -46,12 +47,9 @@ class Profile extends Component {
                                     description="CSE"
                                 />
                             </Card>
-                            <Statistic title="Shares" value={80} />
-                            <Statistic title="Interests" value={9} />
+                            <Statistic title="Shares" className="afterline" value={80} />
+                            <Statistic title="Interests" className="afterline" value={9} />
                             <Statistic title="Posts" value={10} />
-
-
-
                         </div>
                         <Tabs defaultActiveKey="1" centered className="profile-tabs">
                             <TabPane tab="Posts" key="1">
@@ -69,7 +67,7 @@ class Profile extends Component {
                                 Content of Tab Pane 2</TabPane>
                             <TabPane tab="Friends" key="3">
                                 Content of Tab Pane 3</TabPane>
-                            <TabPane tab="Groups" key="4">
+                            <TabPane tab="Groups" className="m-0" key="4">
                                 Content of Tab Pane 4</TabPane>
                         </Tabs>
 
