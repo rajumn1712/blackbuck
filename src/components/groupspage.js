6570a4cf54ef43533577d18ae9ba276c749bfc90
saranya <saranya.ziraff@gmail.com>
2020-11-16 18:09:26 +0530

    Group page changes

--- src/components/groupspage.js
+++ src/components/groupspage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Layout, Card, Avatar, List, Col, Row } from 'antd'
+import { Button, Layout, Card, Avatar, List, Col, Row, Typography } from 'antd'
 import { Link } from 'react-router-dom';
 import { userManager } from '../shared/authentication/auth';
 import { store } from '../store'
@@ -15,18 +15,73 @@ const { Meta } = Card;
 const data = [
     { avatar: Computer }
 ];
+const { Title } = Typography;
 class GroupsPage extends Component {
 
     render() {
         const { user } = store.getState().oidc;
         return (
-            <div className="" >
+            <div className="group-page" >
+                <Row gutter={24} className="mb-16">
+                    <Col className="" span={12}>
+                        <Card
+                            hoverable
+                            cover={<img src={Computer} />} actions={[
+                                <a className="list-link f-14">Jion Group</a>
+                            ]}
+                        >
+                            <Meta title="CSC Champs"
+                                description={<div>Created many changes with CBU
+                                <div className="d-flex align-items-center">
+                                        <span className="list-request">
+                                            <Avatar.Group
+                                                maxCount={4}
+                                                size="large"
+                                                maxStyle={{ color: 'var(--primary)', backgroundColor: 'var(--secondary)' }}
+                                            >
+                                                <Avatar src={user} />
+                                                <Avatar src={user} />
+                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                            </Avatar.Group>
+                                        </span> <span>Mutual Friends</span></div> </div>}
+                            />
+                        </Card>
+                    </Col>
+                    <Col className="" span={12}>
+                        <Card
+                            hoverable
+                            cover={<img src={Computer} />} actions={[
+                                <a className="list-link f-14">Jion Group</a>
+                            ]}>
+                            <Meta title="CSC Champs"
+                                description={<div>Created many changes with CBU
+                                <div className="d-flex align-items-center">
+                                        <span className="list-request">
+                                            <Avatar.Group
+                                                maxCount={4}
+                                                size="large"
+                                                maxStyle={{ color: 'var(--primary)', backgroundColor: 'var(--secondary)' }}
+                                            >
+                                                <Avatar src={user} />
+                                                <Avatar src={user} />
+                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                            </Avatar.Group>
+                                        </span> <span>Mutual Friends</span></div> </div>}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+                <div className="f-16"><Title level={5}>Entertainment</Title></div>
                 <Row gutter={24}>
                     <Col className="" span={12}>
                         <Card
                             hoverable
                             cover={<img src={Computer} />} actions={[
-                                <Button type="primary" >Create a Group</Button>
+                                <a className="list-link f-14">Jion Group</a>
                             ]}
                         >
                             <Meta title="CSC Champs"
@@ -52,9 +107,8 @@ class GroupsPage extends Component {
                         <Card
                             hoverable
                             cover={<img src={Computer} />} actions={[
-                                <Button type="primary" >Create a Group</Button>
-                            ]}
-                        >
+                                <a className="list-link f-14">Jion Group</a>
+                            ]}>
                             <Meta title="CSC Champs"
                                 description={<div>Created many changes with CBU
                                 <div className="d-flex align-items-center">
@@ -74,7 +128,6 @@ class GroupsPage extends Component {
                             />
                         </Card>
                     </Col>
-
                 </Row>
             </div>
 
