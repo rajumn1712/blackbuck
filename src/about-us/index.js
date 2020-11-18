593ad93baa645f3857143828260ed291f11702bd
vishnutrimurthulu <vishnutrimurthulu.ziraff@gmail.com>
2020-11-18 16:32:45 +0530

    Friends page design

--- src/about-us/index.js
+++ src/about-us/index.js
@@ -1,4 +1,11 @@
+import { Col, Row } from 'antd';
 import React, { Component } from 'react';
+import Ads from '../components/ads';
+import Courses from '../components/courses';
+import Friends from '../components/friends';
+import Groups from '../components/groups';
+import Identity from '../components/identity';
+import Invite from '../components/invite';
 
 class Aboutus extends Component {
 
@@ -8,8 +15,17 @@ class Aboutus extends Component {
 
     render() {
         return (
-            <div>
-                <p>About us works</p>
+            <div className="main">
+                <Row gutter={16}>
+                    <Col xs={24} sm={24} md={16} lg={18} xl={18}>
+                        <Friends />
+                    </Col>
+                    <Col xs={24} sm={12} md={8} lg={6} xl={6}>
+                        <Courses />
+                        <Ads />
+                        <Groups />
+                    </Col>
+                </Row>
             </div>
         )
     }
--- src/components/friends.js
+++ src/components/friends.js
@@ -15,7 +15,57 @@ const data = [
 
     {
         avatar: User1,
-        title: 'IT Groups',
+        title: 'St Ann',
+        members: 6,
+    },
+    {
+        avatar: User2,
+        title: 'Mohan',
+        members: 2,
+    },
+    {
+        avatar: User1,
+        title: 'Jain',
+        members: 6,
+    },
+    {
+        avatar: User2,
+        title: 'Poul',
+        members: 2,
+    },
+    {
+        avatar: User1,
+        title: 'Jake',
+        members: 6,
+    },
+    {
+        avatar: User2,
+        title: 'Calvin',
+        members: 2,
+    },
+    {
+        avatar: User1,
+        title: 'Disee',
+        members: 6,
+    },
+    {
+        avatar: User2,
+        title: 'Cruise',
+        members: 2,
+    },
+    {
+        avatar: User1,
+        title: 'John Wik',
+        members: 6,
+    },
+    {
+        avatar: User2,
+        title: 'Brize',
+        members: 2,
+    },
+    {
+        avatar: User1,
+        title: 'Tom',
         members: 6,
     },
     {
@@ -31,36 +81,38 @@ class Friends extends Component {
         const { user } = store.getState().oidc;
         return (
             <div className="custom-card requests">
-                <Card title="Friend Requests" bordered={false} >
-
+                <Card title="Friends" bordered={false} >
                     <List grid={{
-                        gutter: 16, column: 2,
+                        gutter: 16,
+                        column: 2,
                         xs: 1,
-                        sm: 1,
                         md: 2,
                     }}
                         itemLayout="horizontal"
                         dataSource={data}
                         renderItem={item => (
                             <List.Item>
-                                
                                 <List.Item.Meta
                                     avatar={<Avatar className="request-image" src={item.avatar} />}
                                     title={<div className="d-flex align-items-center"><span className="overflow-text">{item.title}</span></div>}
-                                    description={<div className="mt-8 d-flex align-items-center">
-                                        <span className="list-request">
-                                            <Avatar.Group
-                                                maxCount={4}
-                                                size="large"
-                                                maxStyle={{ color: 'var(--primary)', backgroundColor: 'var(--secondary)' }}
-                                            >
-                                                <Avatar src={user} />
-                                                <Avatar src={user} />
-                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
-                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
-                                                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
-                                            </Avatar.Group>
-                                        </span> <span>Mutual Friends</span></div>}
+                                    description={
+                                        <div className="mt-8 d-flex align-items-center">
+                                            <span className="list-request">
+                                                <Avatar.Group
+                                                    maxCount={4}
+                                                    size="large"
+                                                    maxStyle={{ color: 'var(--primary)', backgroundColor: 'var(--secondary)' }}
+                                                >
+                                                    <Avatar src={user} />
+                                                    <Avatar src={user} />
+                                                    <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                                    <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                                    <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                                                </Avatar.Group>
+                                            </span>
+                                            <span>Mutual Friends</span>
+                                        </div>
+                                    }
                                 />
                             </List.Item>
                         )}
