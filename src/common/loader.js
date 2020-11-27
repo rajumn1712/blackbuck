670b27e367afb00b6d2cdfa65a0483592e143033
SubbaReddy <subbareddy@zirafftechnologies.com>
2020-11-27 10:22:12 +0530

    loader calss

--- src/common/loader.js
+++ src/common/loader.js
@@ -4,8 +4,8 @@ import { LoadingOutlined } from '@ant-design/icons';
 
 const antIcon = <LoadingOutlined style={{ fontSize: 40 }} spin />;
 
-const Loader = ()=>{
-    return <div style={{alignItems:"center",flex:1}}><Spin indicator={antIcon}  /></div>
+const Loader = ({className})=>{
+    return <div className={className}><Spin indicator={antIcon}  /></div>
 }
 
 export default Loader;
