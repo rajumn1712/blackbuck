c8a569c6b838d8858c6ac2c196db415bb9ca8107
saranya <saranya.ziraff@gmail.com>
2020-11-29 19:35:09 +0530

    group about design

--- src/shared/components/groupabout.js
+++ src/shared/components/groupabout.js
@@ -1,36 +1,49 @@
 import React, { Component } from 'react';
-import { Button, Card, Divider, Row, Col, Form, Input, Select,List, Avatar } from 'antd'
+import { Button, Card, Divider, Row, Col, Form, Input, Select, List, Avatar } from 'antd'
 import { Link } from 'react-router-dom';
 import { store } from '../../store'
 import '../../index.css';
 import '../../App.css';
 import TextArea from 'antd/lib/input/TextArea';
+import user from '../../styles/images/user.jpg';
 const { Option } = Select;
+const joingroup = <div className="join-grp-title">John Doe <span className="join-grp-txt">has Created a group name is</span> Mech Mantra</div>
 const data = [
     {
         title: 'Public',
-        description:'Anyone can see whos in the group and what they post',
+        description: 'Only members can see whos in the group and what they post',
     },
     {
         title: 'Visible',
-        description:'Anyone can find this group.',
+        description: 'Anyone can find this group.',
     },
     {
         title: 'General Group',
     },
     {
         title: 'History',
-        description:'Group created on April 3, 2018. Name last changed on June 27, 2018. ',
+        description: 'Group created on March 17, 2012  ',
     },
 ];
 class GroupAbout extends Component {
-
+    state = {
+        post: joingroup,
+        users: [
+            { image: user, id: 1, initial: '', colorbc: '' },
+            { image: user, id: 2, initial: '', colorbc: '' },
+            { image: user, id: 3, initial: '', colorbc: '' },
+            { image: '', id: 4, initial: 'NM', colorbc: '#f56a00' },
+            { image: '', id: 5, initial: 'NM', colorbc: '#f56a00' },
+            { image: '', id: 6, initial: 'NM', colorbc: '#f56a00' },
+            { image: '', id: 7, initial: 'NM', colorbc: '#f56a00' }
+        ]
+    }
 
     render() {
         const { user } = store.getState().oidc;
-
+        const grouppost = { ...this.state };
         return (
-            <div className="custom-card">
+            <div className="custom-card group-member">
                 <Card title="About This Group" bordered={false}>
                     <div>
                         <p>Programming is the process of creating a set of instructions that tell a computer how to perform a task</p>
@@ -42,8 +55,8 @@ class GroupAbout extends Component {
                                     <List.Item>
                                         <List.Item.Meta
                                             avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                                            title={<a href="https://ant.design">{item.title}</a>}
-                                            description={<a href="https://ant.design">{item.description}</a>}
+                                            title={item.title}
+                                            description={item.description}
                                         />
                                     </List.Item>
                                 )}
@@ -52,7 +65,72 @@ class GroupAbout extends Component {
 
                     </div>
                 </Card>
-                
+                <Card title="Recommended by the Admins" bordered={false} actions={[
+                    <Button type="primary" >See All Groups</Button>
+                ]}>
+                       
+                        <div>
+                        <List
+                        itemLayout="horizontal"
+                        dataSource={this.state.data}
+                        renderItem={item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Avatar src={item.image} />}
+                                    title={<div className="d-flex align-items-center"><span className="overflow-text" title={item.name}>{item.name}</span></div>}
+                                    description={<div><div className="overflow-text">{item.description}</div>
+                                    <div className="text-overflow">
+                                    <span>
+                                        <span className="mr-4" style={{ color: 'var(--textprimary)',fontWeight: '500' }}>{item.members}</span> 
+                                         Members
+                                    </span> | <span>
+                                        <span className="mr-4" style={{ color: 'var(--textprimary)',fontWeight: '500' }}>5</span> 
+                                         posts
+                                    </span></div>
+                                    </div>}
+                                />
+                                <Link className="ml-8 f-12 list-link ml-16" onClick={() => this.joinGroup(item)}>Join Group</Link>
+                            </List.Item>
+                        )}
+                    />
+                        </div>
+                </Card>
+                <Card title="Members" bordered={false} actions={[
+                    <Button type="primary" >See All</Button>
+                ]}>
+                    <div>
+
+                        <div className=" pb-16">
+                            <Avatar.Group
+                                maxCount={4}
+                                size="large"
+                                maxStyle={{ color: 'var(--primary)', backgroundColor: 'var(--secondary)' }}
+                            >
+                                {grouppost.users.map(user => {
+                                    return <Avatar src={user.image} key={user.id} style={{ backgroundColor: user.colorbc }}>
+                                        {user.image ? null : user.initial}
+                                    </Avatar>
+                                })}
+                            </Avatar.Group>
+                            <p>Gunji, Poojanil and 13 other friends are members.</p>
+                        </div>
+                        <div className="">
+                            <Avatar.Group
+                                maxCount={4}
+                                size="large"
+                                maxStyle={{ color: 'var(--primary)', backgroundColor: 'var(--secondary)' }}
+                            >
+                                {grouppost.users.map(user => {
+                                    return <Avatar src={user.image} key={user.id} style={{ backgroundColor: user.colorbc }}>
+                                        {user.image ? null : user.initial}
+                                    </Avatar>
+                                })}
+                            </Avatar.Group>
+                            <p>Admins</p>
+                        </div>
+
+                    </div>
+                </Card>
 
             </div>
         )
