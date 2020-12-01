886eb1d701bc847b9597e6c2d4ae18cdbedad41e
manoj.ziraff <manoj.ziraff@gmail.com>
2020-12-01 20:28:45 +0530

    emoji

--- src/shared/components/postings/Actions/EmojiActions.js
+++ src/shared/components/postings/Actions/EmojiActions.js
@@ -10,9 +10,9 @@ class EmojiAction extends Component {
         return (
             <a className={`like-emojis ${this.props?.IsUserLikes?'focus':""}`} onClick={(event)=>this.props.clickedEvent(event, 'Likes',0)}>
                 <ul class="l-emojis">
-                    <li><Tooltip title="Love"><a onClick={(event) =>  this.props.clickedEvent(event, 'Love')}><img src={Love} /></a></Tooltip></li>
-                    <li><Tooltip title="Claps"><a onClick={(event) =>  this.props.clickedEvent(event, 'Claps')}><img src={Claps} /></a></Tooltip></li>
-                    <li><Tooltip title="Whistle"><a onClick={(event) =>  this.props.clickedEvent(event, 'Whistles')}><img src={Whistle} /></a></Tooltip></li>
+                    <li><Tooltip title="Love"><a onClick={(event) =>  this.props.clickedEvent(event, 'Love')}><span className="like-icon love" /></a></Tooltip></li>
+                    <li><Tooltip title="Claps"><a onClick={(event) =>  this.props.clickedEvent(event, 'Claps')}><span className="like-icon claps" /></a></Tooltip></li>
+                    <li><Tooltip title="Whistle"><a onClick={(event) =>  this.props.clickedEvent(event, 'Whistles')}><span className="like-icon whistle" /></a></Tooltip></li>
                 </ul>
                 <span className="post-icons like-icon like-emojis"></span>Like</a>
         )
