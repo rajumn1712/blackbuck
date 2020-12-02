fa44b269248df58fb094de9d936149120255c866
prasanna <prasanna@zirafftechnologies.com>
2020-12-02 09:19:35 +0530

    done for post modal out side repeat

--- src/shared/postings/index.js
+++ src/shared/postings/index.js
@@ -303,7 +303,7 @@ class Postings extends Component {
             </div>
          </Card>
          {this.state.commentselection.indexOf(post.id) > -1 && <Comments onUpdate={(prop, value) => { this.updatePost(post, prop, value) }} count={post.commentsCount} postId={post.id} />}
-         {post.type !== 'text' && <PostCardModal postData={postObj} visible={this.state.showModal} closed={() => this.closed()} handleEvent={(e, name, post) => this.handleEvent(e, name, post)} handleActions={(event, type, post) => this.handleActions(event, type, post)} updatePost={(event, type, post) => this.updatePost(event, type, post)} />}
+         {/* {post.type !== 'text' && <PostCardModal postData={postObj} visible={this.state.showModal} closed={() => this.closed()} handleEvent={(e, name, post) => this.handleEvent(e, name, post)} handleActions={(event, type, post) => this.handleActions(event, type, post)} updatePost={(event, type, post) => this.updatePost(event, type, post)} />} */}
       </div>
    }
    showComment = (post) => {
@@ -333,6 +333,7 @@ class Postings extends Component {
          {this.state.allPosts?.map((post, indx) => this.renderPost(post))}
          {this.state.loading && <Loader className="loader-top-middle" />}
          {!this.state.loading && (!this.state.allPosts || this.state.allPosts?.length == 0) && <Empty />}
+         <PostCardModal postData={postObj} visible={this.state.showModal} closed={() => this.closed()} handleEvent={(e, name, post) => this.handleEvent(e, name, post)} handleActions={(event, type, post) => this.handleActions(event, type, post)} updatePost={(event, type, post) => this.updatePost(event, type, post)} />
       </div>
    }
 }
