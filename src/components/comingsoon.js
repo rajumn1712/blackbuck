904c8abd7640aa238b0ac51c401d970098f94024
manoj.ziraff <manoj.ziraff@gmail.com>
2020-12-04 11:10:42 +0530

    coming soon page

--- src/components/comingsoon.js
+++ src/components/comingsoon.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Smiley from '../styles/images/smiley.svg'
+import comingsoon from '../styles/images/coming-soon.png'
 class CommingSoon extends Component {
 
     render() {
         return <div className="coming-soon">
-            <h1 >Coming Soon </h1>
-            <img src={Smiley}/>
+            {/* <h1 >Coming Soon </h1>
+            <img src={Smiley}/> */}
+           <img src={comingsoon} style={{width: '100%'}} />
         </div>
     }
 }
