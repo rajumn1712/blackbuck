86ebe5d6320b77246dbe8428d8ebcbb65e8ff56a
prasanna <>
2020-12-12 13:57:21 +0000

    Group Api

--- src/shared/api/apiServer.js
+++ src/shared/api/apiServer.js
@@ -106,7 +106,7 @@ const getBranchSubjects = (college_id, branch_id) => {
   return apiClient.get(FRIENDS_API + `getSubjectsByBranchId/${college_id}/${branch_id}/1000/0`);
 }
 const saveGroup = (obj) => {
-  return apiClient.post(PROFILE_API + `saveVideoAsProfile`, obj);
+  return apiClient.post(GROUPS_API + `saveGroup`, obj);
 }
 export {
   getFriendSuggestions,
