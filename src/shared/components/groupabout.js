d9f5eb6e35ad66347f693ba99e67c49b30dca335
prasanna <prasanna@zirafftechnologies.com>
2020-12-18 15:42:59 +0530

    done for group about

--- src/shared/components/groupabout.js
+++ src/shared/components/groupabout.js
@@ -4,38 +4,52 @@ import { Link } from 'react-router-dom';
 import { store } from '../../store'
 import '../../index.css';
 import '../../App.css';
+import moment from "moment";
 import TextArea from 'antd/lib/input/TextArea';
 import user from '../../styles/images/user.jpg';
 const { Option } = Select;
 const joingroup = <div className="join-grp-title">John Doe <span className="join-grp-txt">has Created a group name is</span> Mech Mantra</div>
-const data = [
-    {
+const data = {
+
+    "Public": {
         title: 'Public',
-        description: 'Only members can see whos in the group and what they post',
-        img:'left-menu private-icon',
+        description: "Anyone can see who's in the group and what they post.",
+        img: 'icons public-icon',
     },
-    {
+
+
+    "College": {
+        title: 'Public',
+        description: "Anyone can see who's in the group and what they post.",
+        img: 'icons public-icon',
+    },
+
+
+    "Private": {
+        title: 'Private',
+        description: 'Only members can see whos in the group and what they post',
+        img: 'left-menu private-icon',
+    },
+
+
+    "Visible": {
         title: 'Visible',
         description: 'Anyone can find this group.',
-        img: 'left-menu preview-icon',
+        img: 'icons visible-icon',
     },
-    {
-        title: 'General Group',
-        img:'left-menu group-icon',
-    },
-    {
-        title: 'Location',
-        img:'left-menu location-icon',
-    },
-    {
-        title: 'History',
-        description: 'Group created on March 17, 2012  ',
-        img:'left-menu history-icon',
-    },
-];
+
+
+
+    "Hidden": {
+        title: "Hidden",
+        img: "icons hidden-icon",
+        description: "Only members can find this group."
+    }
+    ,
+};
 class GroupAbout extends Component {
     state = {
-        post: joingroup,
+        aboutData: this.props.aboutData,
         users: [
             { image: user, id: 1, initial: '', colorbc: '' },
             { image: user, id: 2, initial: '', colorbc: '' },
@@ -45,20 +59,30 @@ class GroupAbout extends Component {
             { image: '', id: 6, initial: 'NM', colorbc: '#f56a00' },
             { image: '', id: 7, initial: 'NM', colorbc: '#f56a00' }
         ]
-    }
 
+    }
+    componetDidMount() {
+
+    }
+    location = (location, type) => {
+        return {
+            title: location,
+            img: type ? "left-menu history-icon" : "left-menu location-icon",
+        }
+    }
     render() {
         const { user } = store.getState().oidc;
         const grouppost = { ...this.state };
+        const { aboutData } = this.state;
         return (
             <div className="custom-card group-member ">
                 <Card title="About This Group" bordered={false}>
                     <div>
-                        <p>Programming is the process of creating a set of instructions that tell a computer how to perform a task</p>
+                        {aboutData.Description && <p>{aboutData.Description}</p>}
                         <div>
                             <List
                                 itemLayout="horizontal"
-                                dataSource={data}
+                                dataSource={[data[aboutData.Type]]}
                                 renderItem={item => (
                                     <List.Item>
                                         <List.Item.Meta
@@ -69,6 +93,48 @@ class GroupAbout extends Component {
                                     </List.Item>
                                 )}
                             />
+                            {aboutData.Hide && <List
+                                itemLayout="horizontal"
+                                dataSource={[data[aboutData.Hide]]}
+                                renderItem={item => (
+                                    <List.Item>
+                                        <List.Item.Meta
+                                            avatar={<span className={item.img} />}
+                                            title={item.title}
+                                            description={item.description}
+                                        />
+                                    </List.Item>
+                                )}
+                            />
+                            }
+                            {aboutData.Location && <List
+                                itemLayout="horizontal"
+                                dataSource={[this.location(aboutData.Location)]}
+                                renderItem={item => (
+                                    <List.Item>
+                                        <List.Item.Meta
+                                            avatar={<span className={item.img} />}
+                                            title={item.title}
+                                            description={item.description ? item.description : ''}
+                                        />
+                                    </List.Item>
+                                )}
+                            />
+                            }
+                            {aboutData.CreatedDate && <List
+                                itemLayout="horizontal"
+                                dataSource={[this.location(aboutData.CreatedDate, 'History')]}
+                                renderItem={item => (
+                                    <List.Item>
+                                        <List.Item.Meta
+                                            avatar={<span className={item.img} />}
+                                            title={'History'}
+                                            description={(`Group created on ${moment(aboutData.CreatedDate).format('ll')}`)}
+                                        />
+                                    </List.Item>
+                                )}
+                            />
+                            }
                         </div>
 
                     </div>
