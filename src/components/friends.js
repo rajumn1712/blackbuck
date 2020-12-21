dea7e51d572900186a56cc0de15850940a456c80
saranya <saranya.ziraff@gmail.com>
2020-12-21 17:06:40 +0530

    friends tabs

--- src/components/friends.js
+++ src/components/friends.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Avatar, List, Row, Col } from "antd";
+import { Card, Avatar, List, Row, Col,Tabs } from "antd";
 import { store } from "../store";
 import "../index.css";
 import "../App.css";
@@ -8,6 +8,7 @@ import defaultUser from "../styles/images/defaultuser.jpg";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Loader from "../common/loader";
+const { TabPane } = Tabs;
 class Friends extends Component {
   componentDidMount() {
     if (this.props.onRef)
@@ -33,6 +34,102 @@ class Friends extends Component {
     return (
       <div className="custom-card requests">
         {loading && <Loader className="loader-top-middle" />}
+
+        {/*    /// FRIENDS TABS ///   
+        <div className="main">
+        <Row gutter={16}>
+          <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+            <div >
+            <Tabs defaultActiveKey="1"
+              className="group-tabs sub-tab profile-tabs"
+              onChange={(e) => this.setState({ ...this.state, tabkey: e })}>
+              <TabPane className="group-page" tab="Friends" key="1">
+                <Row gutter={16}>
+                  <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                  <List
+            grid={{
+              column: 2,
+              xs: 1,
+              md: 2,
+            }}
+            itemLayout="horizontal"
+            dataSource={FriendsList}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={
+                    <Link to="/commingsoon">
+                      <Avatar
+                        className="request-image"
+                        src={item.Image || defaultUser}
+                      />
+                    </Link>
+                  }
+                  title={
+                    <div className="d-flex align-items-center">
+                      <Link to={"/profileview/" + item.UserId}>
+                        <span className="overflow-text post-title">
+                          {item.Firstname}
+                        </span>
+                      </Link>
+                    </div>
+                  }
+                  description={
+                    <div className="mt-8 d-flex align-items-center">
+                      <span className="list-request">
+                        <Avatar.Group
+                          maxCount={4}
+                          size="large"
+                          maxStyle={{
+                            color: "var(--primary)",
+                            backgroundColor: "var(--secondary)",
+                          }}
+                        >
+                          {item.MutualFriends?.map((friend, index) => {
+                            return (
+                              <Avatar
+                                key={index}
+                                src={friend.Image || defaultUser}
+                              />
+                            );
+                          })}
+                        </Avatar.Group>
+                      </span>
+                      {item.MutualFriends.length > 0 && (
+                        <span>
+                          <span>{item.MutualFriends.length}</span>
+                          <span> Mutual Friends</span>
+                        </span>
+                      )}
+                    </div>
+                  }
+                />
+              </List.Item>
+            )}
+          />
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tab="Invite Friends" key="3">
+                <Row gutter={16}>
+                  <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                 
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tab="Suggested Friends" key="2">
+                <Row gutter={16}>
+                  <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+                  
+                  </Col>
+                </Row>
+              </TabPane>
+            </Tabs>
+            </div>
+          </Col>
+        </Row>
+      </div>  /// FRIENDS TABS /// */}
+
         <Card title={`Friend (${FriendsList.length})`} bordered={true}>
           <List
             grid={{
