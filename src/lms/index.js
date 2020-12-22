ac92814cf35517e4ef03f394e67c89d99e0d008e
vishnutrimurthulu <vishnutrimurthulu.ziraff@gmail.com>
2020-12-22 17:43:57 +0530

    lms courses view design

--- src/lms/index.js
+++ src/lms/index.js
@@ -1,6 +1,7 @@
 import { Col, Row } from "antd";
 import React, { Component } from "react";
 import Ads from "../components/ads";
+import Courses from './courses'
 
 class LMSComponent extends Component {
     render() {
@@ -8,7 +9,7 @@ class LMSComponent extends Component {
             <div className="main">
                 <Row gutter={16}>
                     <Col xs={24} sm={24} md={16} lg={17} xl={17}>
-
+                        <Courses />
                     </Col>
                     <Col xs={24} sm={12} md={8} lg={7} xl={7}>
 
