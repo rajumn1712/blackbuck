3fd77911a94926267a025e61a6242ea239f7de85
SubbaReddy <subbareddy@zirafftechnologies.com>
2021-01-20 08:49:42 +0530

    loader

--- src/lms/coursecards.js
+++ src/lms/coursecards.js
@@ -15,189 +15,190 @@ const { TabPane } = Tabs;
 const { Paragraph } = Typography;
 
 class CourseCards extends Component {
-  state = {
-    courses: [],
-    recentCourses: [],
-    loading: true,
-    page: 1,
-    pageSize: 50
-  };
-  componentDidMount() {
-    this.loadCourses("1");
-  }
-  loadCourses = async key => {
-    key = key == "1" ? "courses" : "recentCourses";
-    const _emptyKey = key == "1" ? "recentCourses" : "courses";
-    this.setState({ ...this.state, [_emptyKey]: [], loading: true });
-    const response = await fetchUserCourses(
-      this.props.profile.Id,
-      this.state.page,
-      this.state.pageSize,
-      key
-    );
-    if (response.ok) {
-      this.setState({ ...this.state, [key]: response.data, loading: false });
-    } else {
-      this.setState({ ...this.state, loading: false });
+    state = {
+        courses: [],
+        recentCourses: [],
+        loading: true,
+        page: 1,
+        pageSize: 50
+    };
+    componentDidMount() {
+        this.loadCourses("1");
     }
-  };
-  renderCard = (card, key) => {};
-  render() {
-    return (
-      <div className="custom-card">
-        <Tabs
-          defaultActiveKey="1"
-          className="profile-tabs"
-          onTabClick={key => {
-            this.loadCourses(key);
-          }}
-        >
-          <TabPane tab="My Courses" key="1">
-            <Card bordered={false}>
-              <div className="px-12 pt-12 pb-8">
-                <Row gutter={16}>
-                  {this.state.courses?.map((course, indx) => (
-                    <Col key={indx} xs={24} md={12} lg={8}>
-                      <Card
-                        className="card-item"
-                        cover={
-                          <img
-                            alt="photography"
-                            src={course.image || photography}
-                          />
-                        }
-                      >
-                        <Meta
-                          title={
-                            <Link
-                              to={"course/" + course.id}
-                              className="text-primary"
-                            >
-                              {course.name}
-                            </Link>
-                          }
-                          description={
-                            <div className="coursecard-cont">
-                              <div>
-                                <Paragraph
-                                  ellipsis={{ rows: 3 }}
-                                  className="f-14 text-primary mb-8"
-                                >
-                                  {course.description}
-                                </Paragraph>
-                                <div>
-                                  <span className="mr-12 f-12 text-secondary">
-                                    <span className="grp-type-icon video-play" />
+    loadCourses = async key => {
+        key = key == "1" ? "courses" : "recentCourses";
+        const _emptyKey = key == "1" ? "recentCourses" : "courses";
+        this.setState({ ...this.state, [_emptyKey]: [], loading: true });
+        const response = await fetchUserCourses(
+            this.props.profile.Id,
+            this.state.page,
+            this.state.pageSize,
+            key
+        );
+        if (response.ok) {
+            this.setState({ ...this.state, [key]: response.data, loading: false });
+        } else {
+            this.setState({ ...this.state, loading: false });
+        }
+    };
+    renderCard = (card, key) => { };
+    render() {
+        return (
+            <div className="custom-card">
+                <Tabs
+                    defaultActiveKey="1"
+                    className="profile-tabs"
+                    onTabClick={key => {
+                        this.loadCourses(key);
+                    }}
+                >
+                    <TabPane tab="My Courses" key="1">
+                        <Card bordered={false}>
+                            <div className="px-12 pt-12 pb-8">
+                                <Row gutter={16}>
+                                    {this.state.courses?.map((course, indx) => (
+                                        <Col key={indx} xs={24} md={12} lg={8}>
+                                            <Card
+                                                className="card-item"
+                                                cover={
+                                                    <img
+                                                        alt="photography"
+                                                        src={course.image || photography}
+                                                    />
+                                                }
+                                            >
+                                                <Meta
+                                                    title={
+                                                        <Link
+                                                            to={"course/" + course.id}
+                                                            className="text-primary"
+                                                        >
+                                                            {course.name}
+                                                        </Link>
+                                                    }
+                                                    description={
+                                                        <div className="coursecard-cont">
+                                                            <div>
+                                                                <Paragraph
+                                                                    ellipsis={{ rows: 3 }}
+                                                                    className="f-14 text-primary mb-8"
+                                                                >
+                                                                    {course.description}
+                                                                </Paragraph>
+                                                                <div>
+                                                                    <span className="mr-12 f-12 text-secondary">
+                                                                        <span className="grp-type-icon video-play" />
                                     10 Videos
                                   </span>
-                                  <span className="f-12 text-secondary">
-                                    <span className="grp-type-icon lessons" />5
+                                                                    <span className="f-12 text-secondary">
+                                                                        <span className="grp-type-icon lessons" />5
                                     Lessons
                                   </span>
-                                </div>
-                              </div>
-                              <Avatar.Group className="group-member mt-12"
-                                maxCount={2}
-                                size="large"
-                                maxStyle={{
-                                  color: 'var(--black)',
-                                  backgroundColor: "#fde3cf",
-                                  fontWeight: 'bold'
-                                }}
-                              >
-                                <Avatar src={defaultUser} />
-                                <Avatar style={{ backgroundColor: 'var(--primary)' }}>
-                                  K
+                                                                </div>
+                                                            </div>
+                                                            <Avatar.Group className="group-member mt-12"
+                                                                maxCount={2}
+                                                                size="large"
+                                                                maxStyle={{
+                                                                    color: 'var(--black)',
+                                                                    backgroundColor: "#fde3cf",
+                                                                    fontWeight: 'bold'
+                                                                }}
+                                                            >
+                                                                <Avatar src={defaultUser} />
+                                                                <Avatar style={{ backgroundColor: 'var(--primary)' }}>
+                                                                    K
                                 </Avatar>
-                                <Tooltip title="Ant User" placement="top">
-                                  <Avatar
-                                    style={{ backgroundColor: "#87d068" }}
-                                    icon={<img src={defaultUser} />}
-                                  />
-                                </Tooltip>
-                                <Avatar
-                                  style={{ backgroundColor: "#1890ff" }}
-                                  icon={<img src={defaultUser} />}
-                                />
-                              </Avatar.Group>
+                                                                <Tooltip title="Ant User" placement="top">
+                                                                    <Avatar
+                                                                        style={{ backgroundColor: "#87d068" }}
+                                                                        icon={<img src={defaultUser} />}
+                                                                    />
+                                                                </Tooltip>
+                                                                <Avatar
+                                                                    style={{ backgroundColor: "#1890ff" }}
+                                                                    icon={<img src={defaultUser} />}
+                                                                />
+                                                            </Avatar.Group>
+                                                        </div>
+                                                    }
+                                                />
+                                            </Card>
+                                        </Col>
+                                    ))}
+                                </Row>
                             </div>
-                          }
-                        />
-                      </Card>
-                    </Col>
-                  ))}
-                </Row>
-              </div>
-            </Card>
-            {this.state.loading && <Loader className="loader-middle" />}
-            {!this.state.loading &&
-              this.state.courses.length === 0 &&
-              this.state.recentCourses.length === 0 && <Empty />}
-          </TabPane>
-          <TabPane tab="Recent Uploads" key="2">
-            <Card bordered={false}>
-              <div className="px-12 pt-12 pb-8">
-                <Row gutter={16}>
-                  {this.state.recentCourses?.map((course, indx) => (
-                    <Col key={indx} xs={24} md={12} lg={8}>
-                      <Card
-                        className="card-item card-height"
-                        cover={
-                          <img
-                            alt="photography"
-                            src={course.image || photography}
-                          />
-                        }
-                        actions={
-                          [
-                            // <Button>Join Course</Button>
-                          ]
-                        }
-                      >
-                        <Meta
-                          title={
-                            <Link
-                              to={"course/" + course.id}
-                              className="text-primary"
-                            >
-                              {course.name}
-                            </Link>
-                          }
-                          description={
-                            <div>
-                              <Paragraph lines={4}>
-                                {course.description}
-                              </Paragraph>
-                              <div>
-                                <span className="mr-12 f-12 text-secondary">
-                                  <span className="grp-type-icon video-play" />
-                                  {course.videos} Videos
+                            {this.state.loading && <Loader className="loader-top-middle" />}
+                            {!this.state.loading &&
+                                this.state.courses.length === 0 &&
+                                this.state.recentCourses.length === 0 && <Empty />}
+                        </Card>
+
+                    </TabPane>
+                    <TabPane tab="Recent Uploads" key="2">
+                        <Card bordered={false}>
+                            <div className="px-12 pt-12 pb-8">
+                                <Row gutter={16}>
+                                    {this.state.recentCourses?.map((course, indx) => (
+                                        <Col key={indx} xs={24} md={12} lg={8}>
+                                            <Card
+                                                className="card-item card-height"
+                                                cover={
+                                                    <img
+                                                        alt="photography"
+                                                        src={course.image || photography}
+                                                    />
+                                                }
+                                                actions={
+                                                    [
+                                                        // <Button>Join Course</Button>
+                                                    ]
+                                                }
+                                            >
+                                                <Meta
+                                                    title={
+                                                        <Link
+                                                            to={"course/" + course.id}
+                                                            className="text-primary"
+                                                        >
+                                                            {course.name}
+                                                        </Link>
+                                                    }
+                                                    description={
+                                                        <div>
+                                                            <Paragraph lines={4}>
+                                                                {course.description}
+                                                            </Paragraph>
+                                                            <div>
+                                                                <span className="mr-12 f-12 text-secondary">
+                                                                    <span className="grp-type-icon video-play" />
+                                                                    {course.videos} Videos
                                 </span>
-                                <span className="f-12 text-secondary">
-                                  <span className="grp-type-icon lessons" />{course.sections}
+                                                                <span className="f-12 text-secondary">
+                                                                    <span className="grp-type-icon lessons" />{course.sections}
                                   Lessons
                                 </span>
-                              </div>
+                                                            </div>
+                                                        </div>
+                                                    }
+                                                />
+                                            </Card>
+                                        </Col>
+                                    ))}
+                                </Row>
                             </div>
-                          }
-                        />
-                      </Card>
-                    </Col>
-                  ))}
-                </Row>
-              </div>
-              {this.state.loading && <Loader className="loader-middle" />}
-              {!this.state.loading &&
-                this.state.courses.length === 0 &&
-                this.state.recentCourses.length === 0 && <Empty />}
-            </Card>
-          </TabPane>
-        </Tabs>
-      </div>
-    );
-  }
+                            {this.state.loading && <Loader className="loader-top-middle" />}
+                            {!this.state.loading &&
+                                this.state.courses.length === 0 &&
+                                this.state.recentCourses.length === 0 && <Empty />}
+                        </Card>
+                    </TabPane>
+                </Tabs>
+            </div>
+        );
+    }
 }
 const mapStateToProps = ({ oidc }) => {
-  return { profile: oidc.profile };
+    return { profile: oidc.profile };
 };
 export default connect(mapStateToProps)(CourseCards);
