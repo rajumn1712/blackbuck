9cee105af2bd90bda11e51b52f6a632ba40c59fa
prasanna <prasanna@zirafftechnologies.com>
2021-02-04 13:23:04 +0530

    done for saved posts

--- src/shared/postings/savedPosts.js
+++ src/shared/postings/savedPosts.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import notify from '../components/notification';
 import Posting from './index'
+import { deleteUserSavedPost } from "../api/postsApi";
 class SavedPostsComponent extends Component {
+    state = {
+        count: 0
+    }
+    deletePost = (post) => {
+        deleteUserSavedPost(post.id).then(() => {
+            let { count } = this.state;
+            count = count + 1;
+            this.setState({ count });
+            notify({ message: "Delete", description: "Saved post deleted successfully" });
+        });
+    };
     render() {
-        return <Posting sharebox={true} friendsSuggestions={false} postingsType="saved" onPostDelete={(post) => { notify({ message: "Delete", description: "This feature will be available in next release",type:'info' }) }} />
+        return <Posting sharebox={true} key={this.state.count} friendsSuggestions={false} postingsType="saved" onPostDelete={(post) => {
+            this.deletePost(post)
+        }} />
     }
 }
 
